7bf12059c96e44da01cbbd95f78dda88a4a3bf06
Marc Katerji <mkaterji@c1r3s4.42beirut.com>
2025-06-29 19:41:21 +0300

    added avatar change api using fastify multipart

--- backend/types/fastify.d.ts
+++ backend/types/fastify.d.ts
@@ -2,13 +2,20 @@
 
 import 'fastify';
 import '@fastify/oauth2';
+import "@fastify/multipart";
 import WebSocket from 'ws';
 import { JWTPayload } from '../utils/jwt';
 import { OAuth2Namespace } from '@fastify/oauth2';
+import { MultipartFile } from "@fastify/multipart";
 
 declare module 'fastify' {
   interface FastifyRequest {
     user: JWTPayload;
+
+    /*  ─── multipart helpers (bring the compiler up to speed) ─── */
+    isMultipart(): boolean;
+    file(opts?: any): Promise<MultipartFile>;
+    files(opts?: any): AsyncIterableIterator<MultipartFile>
   }
 
   interface FastifyInstance {
